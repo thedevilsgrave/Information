08033830a27e5445d3991cc79f6d3d5a1af411f7
local_python <local_python@136.com>
2019-09-10 09:28:26 +0800

    将配置信息抽取到单独的文件

---
+++ config.py
@@ -0,0 +1,22 @@
+from redis import StrictRedis
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+    SECRET_KEY = "P+gASq3hwoH+ZNXDviyzqWscSgpbfDmsgOYwmAWvCoyVRORNFgNiCJBENpWCsloP"
+    # 为Mysql添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 为Redis设置配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    # 为session配置
+    SESSION_TYPE = "redis"
+    # 设置session保存到redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置是否对session进行签名
+    SESSION_USE_SIGNER = True
+    # 设置session是否一直存在，设置过期时间
+    SESSION_PERMANENT = False
+    PERMANENT_SESSION_LIFETIME = 86300 * 2
