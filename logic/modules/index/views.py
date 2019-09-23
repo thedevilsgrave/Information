d7a8126d05299e1b9ef2a8894ddc2afb53f76cf2
local_python <local_python@136.com>
2019-09-23 11:43:42 +0800

    判断用户是否登录的右上角显示

--- logic/modules/index/views.py
+++ logic/modules/index/views.py
@@ -1,17 +1,30 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
+from logic.models import User
 from . import index_blu
 from logic import redis_store
 
 
 @index_blu.route('/')
 def index():
-    # session["name"] = "lishu"
-    # # 测试log日志
-    # logging.debug('测试用的debug')
-    # logging.error('测试用的error')
-    # logging.warning('测试用的warning')
-    return render_template("news/index.html")
+    """显示主页面"""
+    # 1. 如果用户已登录,则将数据库中数据查出替换模板
+    # 1.1 取到用户id
+    user_id = session.get("user_id", None)
+    user = None
+
+    if user_id:
+        # 尝试查询用户模型
+        try:
+            user = User.query.get(user_id)
+        except Exception as err:
+            current_app.logger.error(err)
+
+    data = {
+        "user": user.to_dict() if user else None
+    }
+
+    return render_template("news/index.html", data=data)
 
 
 @index_blu.route("/favicon.ico")
