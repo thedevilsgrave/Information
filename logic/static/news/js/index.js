c4f9bfcd5b0e185ea2a1910740e361cae92d101b
local_python <local_python@136.com>
2019-09-24 12:21:23 +0800

    首页新闻数据展示

--- logic/static/news/js/index.js
+++ logic/static/news/js/index.js
@@ -1,10 +1,12 @@
-var currentCid = 0; // 当前分类 id
+var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
 var data_querying = true;   // 是否正在向后台获取数据
 
 
 $(function () {
+    // 界面加载完成后去请求新闻数据
+    updateNewsData()
     // 首页分类切换
     $('.menu li').click(function () {
         var clickCid = $(this).attr('data-cid')
@@ -46,5 +48,31 @@ $(function () {
 })
 
 function updateNewsData() {
-    // TODO 更新新闻数据
+    var params = {
+        "page": cur_page,
+        "cid": currentCid,
+    }
+    $.get("/news_list", params, function (resp) {
+        if (resp.errno == "2000") {
+            // 先清空原有数据
+            $(".list_con").html('')
+            // 显示数据
+            for (var i=0;i<resp.data.newsList.length;i++) {
+                var news = resp.data.newsList[i]
+                var content = '<li>'
+                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                content += '<div class="author_info fl">'
+                content += '<div class="source fl">来源：' + news.source + '</div>'
+                content += '<div class="time fl">' + news.create_time + '</div>'
+                content += '</div>'
+                content += '</li>'
+                $(".list_con").append(content)
+            }
+        }
+        else {
+            alert(resp.errmsg)
+        }
+    })
 }
