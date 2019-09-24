5dbe0aa9bafc7f6f1922983285fc87605365a4e0
local_python <local_python@136.com>
2019-09-24 11:36:17 +0800

    首页新闻列表后端实现

--- logic/modules/index/views.py
+++ logic/modules/index/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request
+from flask.json import jsonify
 
 from logic.models import User, News
 from . import index_blu
@@ -23,7 +24,7 @@ def index():
     news_list = []
 
     try:
-        news_list = News.query.order_by(News.clicks.desc()).limit(6)
+        news_list = News.query.order_by(News.clicks.desc()).limit(8)
     except Exception as err:
         current_app.logger.error(err)
 
@@ -42,3 +43,55 @@ def index():
 @index_blu.route("/favicon.ico")
 def favicon():
     return current_app.send_static_file("news/favicon.ico")
+
+
+@index_blu.route("/news_list")
+def news_list():
+    """获取首页新闻数据"""
+    # 1. 获取参数
+    cid = request.args.get("cid", "1")
+    page = request.args.get("page", "1")
+    per_page = request.args.get("per_page", "10")
+
+    # 2. 校验参数
+    try:
+        page = int(page)
+        cid = int(cid)
+        per_page = int(per_page)
+    except Exception as er:
+        current_app.logger.error(er)
+        return jsonify(errno="5000", errmsg="参数错误")
+
+    filters = []
+    # 查询的不是最新的数据
+    if cid !=1:
+        # 需要添加条件
+        filters.append(News.category_id == cid)
+    try:
+        # 查数据
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+    except Exception as err:
+        current_app.logger.error(err)
+        return jsonify(errno="5000", errmsg="查询错误")
+
+    # 取当前页的数据
+    new_list = paginate.items
+    # paginate.pages取到总页数
+    page_quantity = paginate.pages
+    wyf_page = paginate.page
+
+    # 将模型对象转换成字典列表
+    news_dict = []
+    for news in new_list:
+        news_dict.append(news.to_basic_dict())
+
+    data = {
+        "totalPage": page_quantity,
+        "newsList": news_dict,
+        "currentPage": wyf_page
+    }
+
+    return jsonify(errno="2000", errmsg="OK", data=data)
+
+
+
