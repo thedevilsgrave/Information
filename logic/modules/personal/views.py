0dfe55b109722cacbca8e936c9e78478743b58d1
local_python <local_python@136.com>
2019-10-08 16:54:44 +0800

    上传头像前端&后端实现&七牛工具使用

--- logic/modules/personal/views.py
+++ logic/modules/personal/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, g, redirect, request, jsonify
 from logic.tools.common import user_login_data
 from logic.modules.personal import user_blu
+from logic.tools.image_storage import storage
 
 
 @user_blu.route("/info")
@@ -40,3 +41,30 @@ def base_info():
     user.signature = signature
 
     return jsonify(errno="2000", errmsg="OK")
+
+
+@user_blu.route("/pic_info", methods=["POST", "GET"])
+@user_login_data
+def pic_info():
+    user = g.user
+    user_avatar_url = "http://oyucyko3w.bkt.clouddn.com/"
+    if request.method == "GET":
+        return render_template("news/user_pic_info.html", data={"user": user.to_dict()})
+    #  如果是POST请求则修改头像
+    # 1. 获取参数
+    try:
+        avatar = request.files.get("avatar").read()
+    except Exception as err:
+        return jsonify(errno="3500", errmsg="参数错误")
+
+    # 2.上传头像
+    try:
+        # 使用自己封装的上传图片的方法
+        key = storage(avatar)
+    except Exception as err:
+        return jsonify(errno="5666", errmsg="上传失败")
+
+    # 3. 保存头像地址
+    user.avatar_url = key
+
+    return jsonify(errno="2000", errmsg="OK", data={"avatar_url": user_avatar_url+key})
