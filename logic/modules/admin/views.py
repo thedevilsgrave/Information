1532889ebdea466eaac5914d7f5ab0106a12f7ed
local_python <local_python@136.com>
2019-10-12 10:09:00 +0800

    添加测试用户&月新增人数&日新增人数

--- logic/modules/admin/views.py
+++ logic/modules/admin/views.py
@@ -1,10 +1,15 @@
-from flask import render_template, request, redirect, current_app, session, url_for
+from datetime import datetime
+
+from flask import render_template, request, redirect, current_app, session, url_for, g
 from logic.models import User
 from logic.modules.admin import admin_blu
+from logic.tools.common import user_login_data
+import time
 
 
 @admin_blu.route("/login", methods=["POST", "GET"])
 def login():
+    """管理员用户登录页"""
     if request.method == "GET":
 
         # 判断用户是否已登录,如果已经登录,则直接跳转到管理员首页
@@ -46,5 +51,48 @@ def login():
 
 
 @admin_blu.route("/index")
+@user_login_data
 def index():
-    return render_template("admin/index.html")
+    """管理员主页显示"""
+    user = g.user
+    if not user:
+        return redirect("admin.login")
+
+    return render_template("admin/index.html", data=user.to_dict())
+
+
+@admin_blu.route("/user_count")
+@user_login_data
+def user_count():
+    user = g.user
+
+    user_total = 0
+    # 用户总数
+    try:
+        user_total = User.query.filter(User.is_admin==False).count()
+    except Exception as err:
+        current_app.logger.error(err)
+
+    # 月新增数
+    mouth_total = 0
+    t = time.localtime()
+    begin_mon_time = datetime.strptime(("%d-%02d-01" % (t.tm_year, t.tm_mon)), "%Y-%m-%d")
+    try:
+        mouth_total = User.query.filter(User.is_admin==False, User.create_time > begin_mon_time).count()
+    except Exception as err:
+        current_app.logger.error(err)
+
+    # 日新增数
+    begin_day_time = datetime.strptime(("%d-%02d-%02d" % (t.tm_year, t.tm_mon, t.tm_mday)), "%Y-%m-%d")
+    day_total = 0
+    try:
+        day_total = User.query.filter(User.is_admin==False, User.create_time > begin_day_time).count()
+    except Exception as err:
+        current_app.logger.error(err)
+
+    data = {
+        "user_total": user_total,
+        "mouth_total": mouth_total,
+        "day_total": day_total
+    }
+    return render_template("admin/user_count.html", data=data)
